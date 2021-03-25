6620ad98a460e0de90995da28cf1784641600a2f
radotooo <radotooo@gmail.com>
2021-03-25 18:24:39 +0200

    refactor: code improvement

--- src/components/MagicHat.js
+++ src/components/MagicHat.js
@@ -1,84 +1,110 @@
 import { Container, Sprite, Text } from 'pixi.js';
 import { random } from '../core/utils';
-
 import gsap from 'gsap';
 
+/**
+ * Initializes a new instance of MagicHat
+ * @class
+ * @extends {PIXI.Container}
+ */
 export default class MagicHat extends Container {
   constructor() {
     super();
     this.name = 'magic-hat';
-
-    this._body = new Sprite.from('hat');
-    this._item = new Text('', { fontSize: 200 });
-    this._itemMask = new Sprite.from('mask');
     this._emojis = ['👻', '💩', '😷', '😇', '🤖', '😂'];
 
+    this._init();
+  }
+
+  /**
+   * @private
+   */
+  _init() {
     this._setBody();
-    this._setMask();
+    this._setItemMask();
     this._setItem();
-
-    this.addChild(this._body);
-    this.addChild(this._item);
-    this.addChild(this._itemMask);
-
     this._addEventListeners();
   }
 
-  animate() {
+  /**
+   * @private
+   */
+  async _animate() {
     this._item.text = this._emojis[Math.floor(random(0, 5))];
+    const tl = new gsap.timeline();
 
-    gsap.to(this._body.scale, {
-      y: 0.9,
-      yoyo: true,
-      repeat: 1,
-      duration: 0.1,
-    });
-
-    gsap.fromTo(
-      this._item,
-      { y: 300 },
-      {
-        y: -100,
-        ease: 'bounce',
-        duration: 1,
-      }
-    );
+    await tl
+      .fromTo(
+        this._body.scale,
+        { y: 1 },
+        {
+          y: 0.9,
+          yoyo: true,
+          repeat: 1,
+          duration: 0.1,
+        }
+      )
+      .fromTo(
+        this._item,
+        { y: 300 },
+        {
+          y: -100,
+          ease: 'bounce',
+          duration: 1,
+        },
+        '<'
+      );
   }
 
   /**
    * @private
    */
   _addEventListeners() {
-    this._body.on('click', () => this.animate());
+    this._body.on('click', () => this._animate());
   }
 
   /**
    * @private
    */
   _setBody() {
-    this._body.anchor.set(1);
-    this._body.interactive = true;
-    this._body.buttonMode = true;
-    this._body.y = 450;
-    this._body.x = 250;
+    const body = new Sprite.from('hat');
+
+    body.anchor.set(1);
+    body.interactive = true;
+    body.buttonMode = true;
+    body.y = 450;
+    body.x = 250;
+
+    this._body = body;
+    this.addChild(this._body);
   }
 
   /**
    * @private
    */
-  _setMask() {
-    this._itemMask.anchor.set(0.5);
-    this._itemMask.x = 35;
-    this._itemMask.y = -30;
+  _setItemMask() {
+    const itemMask = new Sprite.from('mask');
+
+    itemMask.anchor.set(0.5);
+    itemMask.x = 35;
+    itemMask.y = -30;
+
+    this._itemMask = itemMask;
+    this.addChild(this._itemMask);
   }
 
   /**
    * @private
    */
   _setItem() {
-    this._item.anchor.set(0.5);
-    this._item.x = 35;
-    this._item.y = 300;
-    this._item.mask = this._itemMask;
+    const item = new Text('', { fontSize: 200 });
+
+    item.anchor.set(0.5);
+    item.x = 35;
+    item.y = 300;
+    item.mask = this._itemMask;
+
+    this._item = item;
+    this.addChild(this._item);
   }
 }
